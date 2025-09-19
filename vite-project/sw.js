b1e4ebafe4ebaf40fc51cac1742773f4bb7cac88
RubensPaesNeto <rubenspaesneto@gmail.com>
2025-09-19 17:29:40 -0400

    19/09
    feita ate a sw.js

---
+++ vite-project/sw.js
@@ -0,0 +1,18 @@
+import { warmStrategyCache } from 'workbox-recipes';
+import { CacheFirst, StaleWhileRevalidate } from 'workbox-strategies';
+import { registerRoute } from 'workbox-routing';
+import { CacheableResponsePlugin } from 'workbox-cacheable-response';
+import { ExpirationPlugin } from 'workbox-expiration';
+
+// configurando o cache
+const pageCache = new CacheFirst({
+  cacheName: 'primeira-pwa-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 30 * 24 * 60 * 60, // 30 dias
+    }),
+  ],
+});
